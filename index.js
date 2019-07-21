ca39b64782f12d2bf47d1a0ff31a33f64b4cf752
Sergey Demidov <ssevod@gmail.com>
2019-07-22 01:03:54 +0300

    Lesson 76 Joining Arrays

---
+++ index.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3];
+
+const joined = numbers.join(',');
+
+console.log(typeof numbers);
+console.log( joined.split(','));
+
+const messege = 'This is my first message';
+const parts = messege.split('-');
+
+console.log(parts);

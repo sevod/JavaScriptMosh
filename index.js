eced829d35977be98039108d78eb582f043172e8
Sergey Demidov <ssevod@gmail.com>
2019-07-22 22:24:00 +0300

    Filtering an Array

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-//Lesson 77 Sorting Arrays
-const numbers = [2, 3, 1]; 
-numbers.sort();
-//console.log(numbers);
+//Lesson 79 Filtering an Array
+const numbers = [1, -1, 2, 3];
 
-numbers.reverse();
-//console.log(numbers);
-
-const courses = [
-    {id:1, name: 'Node.js'},
-    {id:2, name: 'JavaScript'},
-];
-
-courses.sort(function(a, b){
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
-});
-
-console.log(courses);
-
-const courses2 = [
-    {id:1, name: 'Node.js'},
-    {id:2, name: 'javaScript'}, //J => j
-];
-
-courses2.sort(function(a, b){
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-    if (nameA < nameB) return -1;
-    if (nameA > nameB) return 1;
-    return 0;
-});
-
-console.log(courses2);
+const filtered = numbers.filter(n => n >=0);
+console.log(filtered);

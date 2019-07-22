dfc353cfacd692699193f8a63370664560ba38de
Sergey Demidov <ssevod@gmail.com>
2019-07-22 15:52:04 +0300

    Lesson 77 Sorting Arrays

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const numbers = [1, 2, 3];
+//Lesson 77 Sorting Arrays
+const numbers = [2, 3, 1]; 
+numbers.sort();
+//console.log(numbers);
 
-const joined = numbers.join(',');
+numbers.reverse();
+//console.log(numbers);
 
-console.log(typeof numbers);
-console.log( joined.split(','));
+const courses = [
+    {id:1, name: 'Node.js'},
+    {id:2, name: 'JavaScript'},
+];
 
-const messege = 'This is my first message';
-const parts = messege.split('-');
+courses.sort(function(a, b){
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+});
 
-console.log(parts);
+console.log(courses);
+
+const courses2 = [
+    {id:1, name: 'Node.js'},
+    {id:2, name: 'javaScript'}, //J => j
+];
+
+courses2.sort(function(a, b){
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+});
+
+console.log(courses2);

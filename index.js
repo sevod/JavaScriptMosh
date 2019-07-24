dcfd50e914cdbe52a04ca2e38b23a3d155ed8a6a
Sergey Demidov <ssevod@gmail.com>
2019-07-24 18:45:09 +0300

    Lesson 85 Moving an Element

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-//Lesson 84 Except
+//Lesson 85 Moving an Element
 
-const numbers = [1, 6, 1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 5, 6];
 
-const output = except(numbers, [2, 6]);
+const output = move(numbers, 0, 2);
 
 console.log(output);
 
-//my
-function except(array, excluded) {
-  for (let i = 0; i < excluded.length; i++) {
-    const element = excluded[i];
-    while (array.indexOf(element) != -1)
-      array.splice(array.indexOf(element),1);}
-  return array;
+function move(array, index, offset) {
+    const position = index + offset;
+    if (position < 0 || position >= array.length) console.error ('Error2');
+    else {
+        const output = [...array];
+        const element = output.splice(index, 1)[0];
+        output.splice(position, 0, element);
+        return output;
+    }    
 }
+
+//my Error
+function moveChenge(array, index, offset) {
+    if (index + offset <= 0) console.error ('Error1');
+    else if (index + offset > array.length) console.error ('Error2');
+    else {
+    const a = array[index - 1];
+    const b = array[index + offset - 1];
+    array[index - 1] = b;
+    array[index + offset - 1] = a;
+    return array;
+    }
+}
+
 

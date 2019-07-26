4b8fdc6621a9ae7efe5d6e62654f01067e332093
Sergey Demidov <ssevod@gmail.com>
2019-07-26 20:30:46 +0300

    Lesson 89 Function Declarations vs. Expressions

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-//Lesson 88 Movies
+//Lesson 89 Function Declarations vs. Expressions
 
-const movies = [
-    { title: 'a', year: 2018, rating: 4.5},
-    { title: 'b', year: 2018, rating: 4.7},
-    { title: 'c', year: 2018, rating: 3},
-    { title: 'd', year: 2017, rating: 4.5}
-]
+//Funcion Declaration
+function walk() {
+    console.log('walk');
+}
+walk();
 
+//Anonymous Function Expression
+const run = function () {
+    console.log('run');
+};
+let move = run;
+move();
+run();
 
-
-const titles = movies
-    .filter(m => m.year === 2018 && m.rating >= 4)
-    .sort((a, b) => a.rating - b.rating)
-    .reverse()
-    .map(m => m.title)
-
-
-console.log(titles);
+//Named Function Expression
+let run2 = function run2() {
+    console.log('run2');
+};
+run2();
 
 
 

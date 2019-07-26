320750da81f0dbdc931a6873e025eb4b305c4f81
Sergey Demidov <ssevod@gmail.com>
2019-07-26 21:54:57 +0300

    Lesson 95 Try and Catch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Lesson 94 Getters and Setters
+//Lesson 95 Try and Catch
 
 const person = {
     firstName: 'Mosh',
@@ -7,19 +7,23 @@ const person = {
         return `${person.firstName} ${person.lastName}`;
     },
     set fullName(value){
+        if (typeof value !== 'string')
+            throw new Error('Value is not a string.'); //тут записывается ошибка, данные о которой идут дальше
         const parts = value.split(' ');
+        if (parts.length !== 2)
+            throw new Error('Enter a first and last name');
         this.firstName = parts[0];
         this.lastName = parts[1];
     }
 }
 
-//setters => change (mutate) them
-person.fullName = 'John Smith';
-//getters => access properies
-//console.log(person.fullName);
+try {
+    person.fullName = '';    
+} catch (error) {
+    console.log(error);
+}
+
+
 console.log(person.fullName);
-
-//console.log(`${person.firstName} ${person.lastName}`)
-
 
 
